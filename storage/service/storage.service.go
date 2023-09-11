76e612b20bb3072b6ad4b8886bccccc8fb0001af
mahdi <mahdi.mahdavi.maz@gmail.com>
2023-09-11 13:05:14 +0330

    feat(storage):Storage service is added to the project which is responsible of saving vulnerabilities into the MongoDB database

---
+++ storage/service/storage.service.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"storage/model"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+
+func loadEnvVariables() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error while loading .env file")
+	}
+}
+
+func InitializeStore() {
+	loadEnvVariables()
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable.")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://"+uri))
+	if err != nil {
+		log.Fatalf("Failed to connect to the MongoDB database: %v", err)
+	}
+
+	collection = client.Database("nvd_scrapper").Collection("vulnerabilities")
+}
+
+func SaveVulnerability(vulnerability *model.Vulnerability) bool {
+	result, err := collection.InsertOne(context.TODO(), vulnerability)
+	if err != nil {
+		fmt.Printf("Insertion failed due to some reasons: %v", err)
+		return false
+	}
+
+	fmt.Printf("Inserted document with _id: %v\n", result.InsertedID)
+	return true
+
+}
