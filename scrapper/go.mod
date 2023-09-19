062d012febf1ea57d1ff4a796f46630b288e39fb
mahdi <mahdi.mahdavi.maz@gmail.com>
2023-09-19 14:34:49 +0330

    feat(scrapper): gRPC server is added to the project Some other issues: - FetchVulnerabilities rpc is added to the project which is responsible of extracting vulnerabilities from nvd website - .proto file is added to the project which is a schema for gRPC

--- scrapper/go.mod
+++ scrapper/go.mod
@@ -15,13 +15,16 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/rabbitmq/amqp091-go v1.8.1 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
-	google.golang.org/appengine v1.6.6 // indirect
-	google.golang.org/protobuf v1.24.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230913181813-007df8e322eb // indirect
+	google.golang.org/grpc v1.58.1 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- scrapper/main.go
+++ scrapper/main.go
@@ -1,13 +1,90 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+
+	pb "scrapper/grpc"
 	"scrapper/service"
 )
 
-func main() {
-	query := "mongodb"
+type server struct {
+	pb.UnimplementedScrapperServiceServer
+}
+
+func handlePanic() {
+	a := recover()
+
+	if a != nil {
+		fmt.Printf("Recover from panic: %v", a)
+	}
+}
+
+func (s *server) FetchVulnerabilities(ctx context.Context, req *pb.VulnerabilityRequest) (*pb.VulnerabilityResponse, error) {
+	defer handlePanic()
+
+	query := req.GetName()
+	fmt.Printf("New Request: FetchVulnerabilities rpc is called with query value of '%v' \n", query)
+
 	links := service.ExtractVulnerabilitiesLinks(query)
+
+	if len(links) == 0 {
+		return nil, fmt.Errorf("there was no matching Vulnerabilities")
+	}
+
 	vulnerabilities := service.ExtractVulnerabilitiesDetails(query, links)
-	service.InitializeRabbitMQ()
 	service.SendVulnerabilitiesToDatabase(vulnerabilities)
+
+	result := []*pb.Vulnerability{}
+	for _, vulnerability := range vulnerabilities {
+		result = append(result, &pb.Vulnerability{
+			Name:               vulnerability.Name,
+			CVEID:              vulnerability.CVEID,
+			PublishedDate:      vulnerability.PublishedDate,
+			LastModified:       vulnerability.LastModified,
+			Description:        vulnerability.Description,
+			VulnerableVersions: vulnerability.VulnerableVersions,
+			NVDScore:           vulnerability.NVDScore,
+			CNAScore:           vulnerability.CNAScore,
+		})
+	}
+
+	return &pb.VulnerabilityResponse{
+		Vulnerabilities: result,
+	}, nil
+}
+
+func main() {
+	// Initialize RabbitMQ
+	service.InitializeRabbitMQ()
+
+	lis, tcpErr := net.Listen("tcp", "0.0.0.0:50051")
+	if tcpErr != nil {
+		log.Fatalf("Failed to stablish a tcp connections: %v", tcpErr)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterScrapperServiceServer(grpcServer, &server{})
+
+	channel := make(chan os.Signal, 1)
+	signal.Notify(channel, os.Interrupt)
+
+	go func() {
+		fmt.Println("The scrapper server is started ...")
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("Failed to start gRPC server: %v", err)
+		}
+	}()
+
+	<-channel
+	grpcServer.Stop()
+	fmt.Println("gRPC server is stoped")
+	lis.Close()
+	fmt.Println("TCP connection is closed")
 }
